5aa3aa4b3b3cb225c17c92cfa23dd76da2bf2ddb
Marcel <marcelcruz@live.com>
2020-07-07 12:36:50 -0300

    Add touch events for flat earth

--- flat-earth/script.js
+++ flat-earth/script.js
@@ -4,9 +4,10 @@ const video = document.querySelector('video')
 let isMouseDown = false
 let initialMouseDownXPosition = 0
 let previousX = 0
+let lastMouseOrTouchPositon
 
 function spin(x) {
-  box.style.transform = `rotateY(${x * 0.3}deg)`
+  box.style.transform = `rotateY(${x * 0.4}deg)`
 }
 
 function playVideo() {
@@ -15,35 +16,42 @@ function playVideo() {
   rotationAngle >= 180 ? video.play() : video.pause()
 }
 
-document.addEventListener('mousedown', function (e) {
-  // Start mouse tracking
+function startMoveTrack(e) {
   if (e.target.matches('div.face') || e.target.matches('video.face')) {
     isMouseDown = true
-    initialMouseDownXPosition = e.clientX
+    initialMouseDownXPosition = e.clientX || e.touches[0].clientX
   }
-})
+}
 
-document.addEventListener('mouseup', function (e) {
+function endMoveTrack() {
   if (isMouseDown) {
-    const mousePositionX = e.clientX
-
     // Save how much rotation happened, so next time
     // it continues from there
-    previousX = mousePositionX - initialMouseDownXPosition + previousX
+    previousX = lastMouseOrTouchPositon - initialMouseDownXPosition + previousX
 
     // Reset mouse tracking
     isMouseDown = false
     initialMouseDownXPosition = 0
   }
-})
+}
 
-document.addEventListener('mousemove', function (e) {
+function trackMovement(e) {
   if (isMouseDown) {
-    const mousePositionX = e.clientX
+    const mousePositionX = e.clientX || e.touches[0].clientX
 
     const movedX = mousePositionX - initialMouseDownXPosition + previousX
+    lastMouseOrTouchPositon = mousePositionX
 
     spin(movedX)
     playVideo()
   }
-})
+}
+
+document.addEventListener('mousedown', e => startMoveTrack(e))
+document.addEventListener('touchstart', e => startMoveTrack(e))
+
+document.addEventListener('mouseup', () => endMoveTrack())
+document.addEventListener('touchend', () => endMoveTrack())
+
+document.addEventListener('mousemove', e => trackMovement(e))
+document.addEventListener('touchmove', e => trackMovement(e))
