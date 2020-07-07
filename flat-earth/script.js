95cb67c4164d53c00e364452164cbd07134f5ee9
Marcel <marcelcruz@live.com>
2020-07-07 10:45:29 -0300

    Add video

--- flat-earth/script.js
+++ flat-earth/script.js
@@ -1,46 +1,47 @@
-const box = document.querySelector("section")
+const box = document.querySelector('section')
+const video = document.querySelector('video')
 
 let isMouseDown = false
-
 let initialMouseDownXPosition = 0
-let initialMouseDownYPosition = 0
-
 let previousX = 0
-let previousY = 0
 
-function spin(x, y) {
-  box.style.transform = `rotateX(${y * -0.5}deg) rotateY(${x * 0.5}deg)`
+function spin(x) {
+  box.style.transform = `rotateY(${x * 0.5}deg)`
 }
 
-document.addEventListener("mousedown", function (e) {
+function playVideo() {
+  const rotationAngle = getRotationAngle(box)
+
+  rotationAngle >= 180 ? video.play() : video.pause()
+}
+
+document.addEventListener('mousedown', function (e) {
+  //   console.log('down ->')
+  // Start mouse tracking
   isMouseDown = true
   initialMouseDownXPosition = e.clientX
-  initialMouseDownYPosition = e.clientY
 })
 
-document.addEventListener("mouseup", function (e) {
+document.addEventListener('mouseup', function (e) {
+  //   console.log('up ->')
   const mousePositionX = e.clientX
-  const mousePositionY = e.clientY
 
   // Save how much rotation happened, so next time
   // it continues from there
   previousX = mousePositionX - initialMouseDownXPosition + previousX
-  previousY = mousePositionY - initialMouseDownYPosition + previousY
 
   // Reset mouse tracking
   isMouseDown = false
   initialMouseDownXPosition = 0
-  initialMouseDownYPosition = 0
 })
 
-document.addEventListener("mousemove", function (e) {
+document.addEventListener('mousemove', function (e) {
   if (isMouseDown) {
     const mousePositionX = e.clientX
-    const mousePositionY = e.clientY
 
-    const xMoved = mousePositionX - initialMouseDownXPosition + previousX
-    const yMoved = mousePositionY - initialMouseDownYPosition + previousY
+    const movedX = mousePositionX - initialMouseDownXPosition + previousX
 
-    spin(xMoved, yMoved)
+    spin(movedX)
+    playVideo()
   }
 })
